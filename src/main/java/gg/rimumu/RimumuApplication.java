bf1056d6fca134117de1cf3b892d84ecab308419
CodeRothy <yiwooy@naver.com>
2022-07-29 15:05:57 +0900

    Initial commit

---
+++ src/main/java/gg/rimumu/RimumuApplication.java
@@ -0,0 +1,13 @@
+package gg.rimumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimumuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimumuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gg/rimumu/RimumuApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.rimumu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RimumuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
