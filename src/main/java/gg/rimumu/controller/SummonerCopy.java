0520735ac0c0edc3a42db15882a1b4eead5162b6
CodeRothy <yiwooy@naver.com>
2022-12-07 23:41:34 +0900

    2글자 소환사 검색 오류 해결
    - 2글자 초과 소환사는 공백과 상관없이 검색되나, 2글자 소환사는 가운데 공백 제거 불가

--- src/main/java/gg/rimumu/controller/SummonerCopy.java
+++ src/main/java/gg/rimumu/controller/SummonerCopy.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -32,13 +33,15 @@ public class SummonerCopy {
 
     // 소환사 Summoner(smn) 검색
     @GetMapping("/summoner")
-    public String summoner(@RequestParam("smn") String smn, SummonerDto summonerDto, Model model) throws ParseException, IOException, java.text.ParseException {
+    public String summoner(@RequestParam("smn") String smn, SummonerDto summonerDto, Model model) throws ParseException, IOException {
 
-        smn.replaceAll(" ", "");//검색 명 공백제거
-        model.addAttribute("smn", smn);
+        smn = smn.trim();
+        if (smn.length() < 3){ // 2글자 소환사는 가운데에 공백 꼭 필요
+            smn = smn.substring(0,1) + " " + smn.substring(1);
+        }
+        smn = URLEncoder.encode(smn, "utf-8");
 
         summonerDto = summonerService.smnSearch(summonerDto, smn);
-
 
         model.addAttribute("summonerDto", summonerDto);
         return "summoner/smnResult";
