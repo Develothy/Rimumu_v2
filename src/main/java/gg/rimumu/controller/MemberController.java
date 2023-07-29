6290f2af32c60c91e8fce7e5e93450ae8452c5bf
rothy <rothy@lezhin.com>
2023-07-30 03:21:47 +0900

    feat : Encrypt test

--- src/main/java/gg/rimumu/controller/MemberController.java
+++ src/main/java/gg/rimumu/controller/MemberController.java
@@ -1,6 +1,7 @@
 package gg.rimumu.controller;
 
 import gg.rimumu.dto.Member;
+import gg.rimumu.exception.RimumuException;
 import gg.rimumu.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.ObjectUtils;
@@ -19,7 +20,13 @@ public class MemberController {
         if (ObjectUtils.isEmpty(member)) {
             return "회원가입 정보를 정확히 입력해주세요!";
         }
-        return memberService.createMember(member);
+
+        try {
+            return memberService.createMember(member);
+        } catch (Exception e) {
+            return "로그인에 실패하였습니다." + e.getMessage();
+        }
+
     }
 
     @PostMapping
@@ -32,12 +39,10 @@ public class MemberController {
             return "아이디 또는 비밀번호가 입력되지 않았습니다.";
         }
         try {
-            memberService.login(email, password);
+            return memberService.login(email, password);
         } catch (Exception e) {
             return "로그인에 실패하였습니다." + e.getMessage();
         }
-
-        return "로그인 성공";
     }
 
 }
--- src/main/java/gg/rimumu/exception/ErrorMsg.java
+++ src/main/java/gg/rimumu/exception/ErrorMsg.java
@@ -3,6 +3,7 @@ package gg.rimumu.exception;
 public enum ErrorMsg {
 
     ServerException("서버와 연결이 올바르지 않습니다. 잠시 후 다시 시도 해주세요."),
+    EncryptException(" 암.복호화에 실패하였습니다."),
 
     SummonerNotFound("님은 존재하지 않는 소환사입니다."),
     MatchNotFound(" match가 존재하지 않습니다."),
